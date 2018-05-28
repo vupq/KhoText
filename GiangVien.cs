331b4d084e0d70a80bbf4466b705f821c23cf7b6
lmtlmt <38099927+lmtlmt@users.noreply.github.com>
2018-05-28 21:36:01 +0700

    Add files via upload

---
+++ GiangVien.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueObject
+{
+    public class GiangVien
+    {
+        public int MaGV { get; set; }
+        public string TenGV { get; set; }
+        public string SDT { get; set; }
+        public string Email { get; set; }
+        public string MoTa { get; set; }
+    }
+}
